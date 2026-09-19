587325bf1c1d84dca86a3ea9a067fff0d8496fba
ryan-neville <108026531+ryan-neville@users.noreply.github.com>
2026-09-19 14:52:49 -0400

    feat: Added 4 more phrases.

--- app/data/flashcards.ts
+++ app/data/flashcards.ts
@@ -341,6 +341,10 @@ export const greetings = phraseDeck("greetings", [
   ["わかりました", "Wakarimashita", "I understand / Got it"],
   ["英語を話せますか?", "Eigo o hanasemasu ka?", "Do you speak English?"],
   ["日本語があまりわかりません", "Nihongo ga amari wakarimasen", "I don't speak Japanese well"],
+  ["いえいえ", "Ie ie", "No, no. / Not at all."],
+  ["いえいえ、まだまだです", "Ie ie, mada mada desu", "Oh no, I still have a long way to go."],
+  ["いえいえ、大丈夫ですよ", "Ie ie, daijōbu desu yo", "No, no, it's totally fine."],
+  ["とんでもないです", "Tondemonai desu", "It was nothing at all. / Not at all."],
 ]);
 
 export const gettingAround = phraseDeck("getting-around", [
